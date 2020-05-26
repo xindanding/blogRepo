91962acd09b5fb2acf2df1a860bfd00a49de2b61
dandingge <1752605355@qq.com>
2020-05-26 23:52:05 +0800

    提及

---
+++ myeblog1/src/main/java/com/example/myeblog/MyeblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.myeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyeblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyeblogApplication.class, args);
+	}
+
+}
